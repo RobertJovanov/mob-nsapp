90b4997412ca4be3fa62ff67e81ac7fd78a1e00d
sindhujaaruljothi <sindhu.shil@gmail.com>
2019-10-07 14:02:56 +0200

    Create project

---
+++ mobtravel/src/main/java/com/mobiquity/mobtravelapp/MobtravelappApplication.java
@@ -0,0 +1,13 @@
+package com.mobiquity.mobtravelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobtravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobtravelApplication.class, args);
+	}
+
+}
---
+++ mobtravel/src/test/java/com/mobiquity/mobtravelapp/MobtravelappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mobiquity.mobtravelapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MobtravelappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
