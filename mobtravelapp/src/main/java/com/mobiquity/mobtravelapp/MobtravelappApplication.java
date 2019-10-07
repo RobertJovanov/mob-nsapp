73e79ca9ae322eea1bdb42eec4709e5891e8dcac
sindhujaaruljothi <sindhu.shil@gmail.com>
2019-10-07 14:17:54 +0200

    Create a spring project with packages

---
+++ mobtravelapp/src/main/java/com/mobiquity/mobtravelapp/MobtravelappApplication.java
@@ -0,0 +1,13 @@
+package com.mobiquity.mobtravelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobtravelappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobtravelappApplication.class, args);
+	}
+
+}
