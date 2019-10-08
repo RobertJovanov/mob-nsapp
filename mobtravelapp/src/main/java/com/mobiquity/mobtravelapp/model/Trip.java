0e04e970f14836944df8b0635cf6314edf5a6bcc
Marko Stanoevich <MStanoevich@gmail.com>
2019-10-08 17:02:49 +0200

    Create Trip Class

--- mobtravelapp/src/main/java/com/mobiquity/mobtravelapp/model/Trip.java
+++ mobtravelapp/src/main/java/com/mobiquity/mobtravelapp/model/Trip.java
@@ -1,5 +1,21 @@
 package com.mobiquity.mobtravelapp.model;
 
-public class Trip {
+import java.util.List;
 
+public class Trip {
+    private String origin;
+    private String destination;
+    private String dateTime;
+    private List<Route> routes;
+
+    private Trip(String origin, String destination, String dateTime, List<Route> routes){
+        this.origin = origin;
+        this.destination = destination;
+        this.dateTime = dateTime;
+        this.routes = routes;
+    }
+
+    public static Trip createTrip(String origin, String destination, String dateTime, List<Route> routes){
+        return new Trip(origin, destination, dateTime, routes);
+    }
 }
