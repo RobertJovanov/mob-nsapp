9fa326a02eaef71701ebf59934131b649a88d36b
payal pureha <ppureha@mobiquityinc.com>
2019-10-16 10:08:17 +0200

    Add documentaion for WeatherService

--- mobtravelapp/src/main/java/com/mobiquity/mobtravelapp/service/WeatherService.java
+++ mobtravelapp/src/main/java/com/mobiquity/mobtravelapp/service/WeatherService.java
@@ -3,7 +3,6 @@ package com.mobiquity.mobtravelapp.service;
 
 import com.mobiquity.mobtravelapp.model.WeatherModel.Weather;
 import com.mobiquity.mobtravelapp.model.travelModel.Station;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,6 +15,12 @@ public class WeatherService {
     private String uri="https://api.darksky.net/forecast/{0}/{1},{2},{3}";
     final String key = System.getenv("DARKSKYAPIKEY");
 
+    /**
+     * Extract weather for Origin and Destination station
+     * @param station
+     * @param dateTime
+     * @return
+     */
 
     public Weather getWeather(Station station, String dateTime) {
         String url = MessageFormat.format(uri, key, station.getLatitude(), station.getLongitude(), dateTime);
