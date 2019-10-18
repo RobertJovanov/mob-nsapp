7f44f9ee58731167cf54869ff566daacb8b3980e
Marko Stanoevich <MStanoevich@gmail.com>
2019-10-18 14:44:50 +0200

    Refactor code

--- mobtravelapp/src/main/java/com/mobiquity/mobtravelapp/controller/TravelController.java
+++ mobtravelapp/src/main/java/com/mobiquity/mobtravelapp/controller/TravelController.java
@@ -4,19 +4,16 @@ import com.mobiquity.mobtravelapp.model.travelModel.RouteModel;
 import com.mobiquity.mobtravelapp.model.travelModel.Trip;
 import com.mobiquity.mobtravelapp.service.TravelService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("/travelInfo")
 public class TravelController {
 
     @Autowired
     TravelService travelService;
 
-    //@GetMapping("/routes")
     @RequestMapping("/travelInfo/routes")
     public Trip getRoutes(@RequestParam(value = "fromStation") String fromStation, @RequestParam(value = "toStation") String toStation,
                           @RequestParam(value = "dateTime", defaultValue = "") String dateTime) throws Exception {
@@ -27,6 +24,5 @@ public class TravelController {
     public String checkHealth(){
         return "We are fine and dandy\n";
     }
-
 
 }
